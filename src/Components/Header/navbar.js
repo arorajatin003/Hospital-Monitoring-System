0d42df9c66cf12f49c3ab28e7827bacb77e36d16
Anupam <anupam.saini18@gmail.com>
2021-12-26 19:40:24 +0530

    css updated

--- src/Components/Header/navbar.js
+++ src/Components/Header/navbar.js
@@ -17,11 +17,11 @@ return(
             <form>
               <div className="input-group no-border">
                 <input type="text" value="" className="form-control" placeholder="Search..."/>
-                <div className="input-group-append">
+                {/* <div className="input-group-append">
                   <div className="input-group-text">
                     <i className="nc-icon nc-zoom-split"></i>
                   </div>
-                </div>
+                </div> */}
               </div>
             </form>
           </div>
